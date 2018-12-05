bf8f7c57f6d10e14fb8475cfb77a623be9126a9c
Matheus Bai <mathios@Matheuss-MacBook-Pro.local>
2018-12-05 11:14:47 -0500

    first commit

---
+++ express_server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+const urlDB = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDB);
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>');
+});
+
+app.get('/urls', (req, res) => {
+  res.render('urls_index', { urls: urlDB });
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/urls/:id', (req, res) => {
+  let urls = { shortURL: req.params.id, longURL: urlDB[req.params.id] };
+  res.render('urls_show', urls);
+});
+
+app.post('/urls', (req, res) => {
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL;
+  urlDB[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  let shortURL = req.params.shortURL;
+  if (urlDB[shortURL]) {
+    let longURL = urlDB[shortURL];
+    res.redirect(longURL);
+  } else {
+    res.status(404);
+    res.send("Page not found");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on ${PORT}`);
+});
+
+
+function generateRandomString() {
+  const alphaNumchars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  let randomStr = "";
+  for (let i = 0; i < 6; i++) {
+    let randomNum = Math.floor(Math.random() * alphaNumchars.length);
+    randomStr += alphaNumchars[randomNum];
+  }
+  return randomStr;
+}
